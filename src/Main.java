7369ac5b9825dcefae526074004ec27201e67386
Leroy <LeraRu@tut.by>
2024-11-20 00:56:54 +0300

    Homework Cycle FOR is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задача 1
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
+
+        //Задача 2
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Итерация цикла " + i);
+        }
+
+        //Задача 3
+
+        for (int i = 2; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
+
+        //Задача 4
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        //Задача 5
+
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год явдяется высокосным");
+        }
+
+        //Задача 6
+
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+
+        //Задача 7
+
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
+        }
+
+        //Задача 8
+
+        int total = 29000;
+        int monthTotal = 0;
+        for (int i = 1; i <= 12; i++) {
+            monthTotal = total + monthTotal;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + monthTotal + " рублей");
+        }
+
+        //Задача 9
+
+        int total1 = 29000;
+        int monthTotal1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            monthTotal1 = monthTotal1 + total1/100;
+            System.out.println(" Месяц " + i + ", сумма накоплений равна " + monthTotal1 + " рублей");
+        }
+
+        //Задача 10
+
+        int number = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * number);
+        }
     }
 }
